4c48564098992df9c912bf35d7df84f48d539250
Kevin <stradtkt22@gmail.com>
2019-04-18 22:54:05 -0400

    Added some of the hospitals methods

---
+++ hospital.py
@@ -0,0 +1,24 @@
+class Patient(object):
+    PATIENT_COUNT = 0
+    def __init__(self, name, allergies):
+        self.name = name
+        self.allergies = allergies
+        self.id = Patient.PATIENT_COUNT
+        self.bed_num = None
+        Patient.PATIENT_COUNT += 1
+
+class Hospital(object):
+    def __init__(self,name,cap):
+        self.name = name
+        self.cap = cap
+        self.patients = []
+        self.beds = self.initialize_beds()
+
+    def initialize_beds(self):
+        beds = []
+        for i in range(0, self.cap):
+            beds.append({
+                "bed_id": 1,
+                "Available": True
+            })
+        return beds
